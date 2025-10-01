90ef3b2dcd367d182b2a3cccd823dc46339567ad
anas0723 <anassoomro23@gmail.com>
2025-10-01 17:45:58 +0500

    anas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className='class="w-full mt-8 text-center py-4 text-gray-300'>
-        Made With ❤️ by Muhammad Anas Soomro (me).
+      <footer className='w-full mt-8 text-center py-4 text-gray-300'>
+        Made With 💝 by Muhammad Anas Soomro (me).
       </footer>
     </>
   );
